23c87340de235d27351dd537c2f42cf9ea314b7f
Dustin Catap <cadn@erni.ph>
2018-06-30 19:18:35 +0800

    Removed application of effect in Xamarin Forms project.

--- XF.Material/XF.Material/Effects/MaterialTypographyEffect.cs
+++ XF.Material/XF.Material/Effects/MaterialTypographyEffect.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Xamarin.Forms;
-using XF.Material.Resources;
 using XF.Material.Resources.Typography;
 
 namespace XF.Material.Effects
@@ -20,24 +19,5 @@ namespace XF.Material.Effects
         public double LetterSpacing { get; }
 
         public MaterialTypeScale TypeScale { get; }
-
-        //public override void ApplyEffect()
-        //{
-        //    switch (this.TypeScale)
-        //    {
-        //        case MaterialTypeScale.Body1:
-        //            this.MaterialElement.SetDynamicResource(Label.FontFamilyProperty, MaterialConstants.MATERIAL_FONTFAMILY_REGULAR);
-        //            this.MaterialElement.SetDynamicResource(Label.FontSizeProperty, MaterialConstants.MATERIAL_FONTSIZE_BODY1);
-        //            break;
-        //        case MaterialTypeScale.Body2:
-        //            this.MaterialElement.SetDynamicResource(Label.FontFamilyProperty, MaterialConstants.MATERIAL_FONTFAMILY_REGULAR);
-        //            this.MaterialElement.SetDynamicResource(Label.FontSizeProperty, MaterialConstants.MATERIAL_FONTSIZE_BODY2);
-        //            break;
-        //        case MaterialTypeScale.Button:
-        //            //this.MaterialElement.SetDynamicResource(Button.FontFamilyProperty, MaterialConstants.MATERIAL_FONTFAMILY_MEDIUM);
-        //            //this.MaterialElement.SetDynamicResource(Button.FontSizeProperty, MaterialConstants.MATERIAL_FONTSIZE_BUTTON);
-        //            break;
-        //    }
-        //}
     }
 }
