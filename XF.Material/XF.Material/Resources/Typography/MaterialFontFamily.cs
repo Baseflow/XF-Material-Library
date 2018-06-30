1b348bae8a65b78d8c4112c5735682fdedae7144
Dustin Catap <cadn@erni.ph>
2018-06-30 19:29:15 +0800

    Revert "Revert "Added null checking during GetValue(BindableProperty).""
    This reverts commit 83e56067bdb86948dc8c2ca8956cb2135b20caca.

--- XF.Material/XF.Material/Resources/Typography/MaterialFontFamily.cs
+++ XF.Material/XF.Material/Resources/Typography/MaterialFontFamily.cs
@@ -4,25 +4,25 @@ namespace XF.Material.Resources.Typography
 {
     public class MaterialFontFamily : BindableObject
     {
-        public static readonly BindableProperty RegularProperty = BindableProperty.Create(nameof(Regular), typeof(string), typeof(string));
-        public static readonly BindableProperty MediumProperty = BindableProperty.Create(nameof(Medium), typeof(string), typeof(string));
-        public static readonly BindableProperty BoldProperty = BindableProperty.Create(nameof(Bold), typeof(string), typeof(string));
+        public static readonly BindableProperty RegularProperty = BindableProperty.Create(nameof(Regular), typeof(string), typeof(MaterialFontFamily));
+        public static readonly BindableProperty MediumProperty = BindableProperty.Create(nameof(Medium), typeof(string), typeof(MaterialFontFamily));
+        public static readonly BindableProperty BoldProperty = BindableProperty.Create(nameof(Bold), typeof(string), typeof(MaterialFontFamily));
 
         public string Regular
         {
-            get => GetValue(RegularProperty).ToString();
+            get => GetValue(RegularProperty)?.ToString();
             set => SetValue(RegularProperty, value);
         }
 
         public string Medium
         {
-            get => GetValue(MediumProperty).ToString();
+            get => GetValue(MediumProperty)?.ToString();
             set => SetValue(MediumProperty, value);
         }
 
         public string Bold
         {
-            get => GetValue(BoldProperty).ToString();
+            get => GetValue(BoldProperty)?.ToString();
             set => SetValue(BoldProperty, value);
         }
     }
