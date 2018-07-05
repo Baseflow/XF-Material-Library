375accfb40d8473d54f06df78df91db869348d83
Dustin Catap <cadn@erni.ph>
2018-07-05 16:41:45 +0800

    Added back button handler for android. MaterialDialog can now be dismissed using back button, while MaterialLoading dialog cannot.

--- XF.Material/XF.Material.Droid/Material.cs
+++ XF.Material/XF.Material.Droid/Material.cs
@@ -1,14 +1,19 @@
 ﻿using Android.Content;
 using Android.OS;
 using Android.Support.V7.App;
-using XF.Material.Resources;
+using System;
+using System.Linq;
+using Rg.Plugins.Popup;
+using Rg.Plugins.Popup.Services;
+using System.Threading.Tasks;
 
 namespace XF.Material.Droid
 {
     public static class Material
     {
-        public static bool IsLollipop;
-        public static Context Context;
+        public static Context Context { get; set; }
+
+        public static bool IsLollipop { get; set; }
 
         public static void Init(Context context, Bundle bundle)
         {
@@ -17,6 +22,22 @@ namespace XF.Material.Droid
 
             AppCompatDelegate.CompatVectorFromResourcesEnabled = true;
             Rg.Plugins.Popup.Popup.Init(context, bundle);
+        }
+
+        public static void HandleBackButton(Action backAction)
+        {
+            var materialDialog = PopupNavigation.Instance.PopupStack.FirstOrDefault(p => p is XF.Material.Dialogs.MaterialDialog);
+            var loadingDialog = PopupNavigation.Instance.PopupStack.FirstOrDefault(p => p is XF.Material.Dialogs.MaterialLoadingDialog);
+
+            if (Popup.SendBackPressed(backAction) && materialDialog != null && loadingDialog == null)
+            {
+                PopupNavigation.Instance.RemovePageAsync(materialDialog);
+            }
+
+            else if (Popup.SendBackPressed(backAction) && materialDialog == null && loadingDialog == null)
+            {
+                backAction?.Invoke();
+            }
         }
     }
 }
