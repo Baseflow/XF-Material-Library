29852890751af6c91daabff13831fe39da567da4
Dustin Catap <cadn@erni.ph>
2018-07-09 13:33:29 +0800

    Added MaterialNavigationPage

---
+++ XF.Material/XF.Material.Droid/Renderers/MaterialNavigationPageRenderer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XF.Material.Droid.Renderers;
+using XF.Material.Views;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
+
+[assembly: ExportRenderer(typeof(MaterialNavigationPage), typeof(MaterialNavigationPageRenderer))]
+namespace XF.Material.Droid.Renderers
+{
+    public class MaterialNavigationPageRenderer : Xamarin.Forms.Platform.Android.AppCompat.NavigationPageRenderer
+    {
+        private MaterialNavigationPage _navigationPage;
+        private Toolbar _toolbar;
+
+        public MaterialNavigationPageRenderer(Context context) : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<NavigationPage> e)
+        {
+            base.OnElementChanged(e);
+
+            if(e?.NewElement != null)
+            {
+                _navigationPage = this.Element as MaterialNavigationPage;
+                _toolbar = this.ViewGroup.GetChildAt(0) as Toolbar;
+
+                if(_navigationPage.HasShadow)
+                {
+                    _toolbar.Elevate(8);
+                }
+            }
+        }
+    }
+}
--- XF.Material/XF.Material/Views/MaterialNavigationpage.cs
+++ XF.Material/XF.Material/Views/MaterialNavigationpage.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
+using XF.Material.Resources;
 
 namespace XF.Material.Views
 {
     public class MaterialNavigationPage : NavigationPage
     {
+        public static readonly BindableProperty HasShadowProperty = BindableProperty.Create(nameof(HasShadow), typeof(bool), typeof(MaterialNavigationPage), true);
+
+        public bool HasShadow
+        {
+            get => (bool)GetValue(HasShadowProperty);
+            set => SetValue(HasShadowProperty, value);
+        }
+
         public MaterialNavigationPage(Page rootPage) : base(rootPage)
         {
-            this.SetDynamicResource(BarBackgroundColorProperty, "Material.Color.Primary");
+            this.SetDynamicResource(BarBackgroundColorProperty, MaterialConstants.MATERIAL_COLOR_PRIMARY);
+            this.SetDynamicResource(BarTextColorProperty, MaterialConstants.MATERIAL_COLOR_ONPRIMARY);
+            this.SetDynamicResource(BackgroundColorProperty, MaterialConstants.MATERIAL_COLOR_BACKGROUND);
         }
     }
 }
