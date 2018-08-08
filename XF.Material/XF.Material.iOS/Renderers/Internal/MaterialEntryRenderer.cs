8622d14990661cb765eeefcf94ba6ec3e0ece81c
Dustin Catap <cadn@erni.ph>
2018-08-08 15:33:18 +0800

    Added MaterialTextField.

---
+++ XF.Material/XF.Material.iOS/Renderers/Internal/MaterialEntryRenderer.cs
@@ -0,0 +1,22 @@
+﻿using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XF.Material.iOS.Renderers.Internal;
+using XF.Material.Views.Internal;
+
+[assembly: ExportRenderer(typeof(MaterialEntry), typeof(MaterialEntryRenderer))]
+namespace XF.Material.iOS.Renderers.Internal
+{
+    internal class MaterialEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if(e?.NewElement != null)
+            {
+                this.Control.BorderStyle = UITextBorderStyle.None;
+            }
+        }
+    }
+}
---
+++ XF.Material/XF.Material/Views/Internal/MaterialEntry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF.Material.Views.Internal
+{
+    /// <summary>
+    /// Used for rendering the <see cref="Xamarin.Forms.Entry"/> control in <see cref="XF.Material.Views.MaterialTextField"/>.
+    /// </summary>
+    public sealed class MaterialEntry : Entry { }
+}
