0f052b1101148b2a588ab31bf9d713894ce3c127
Dustin Catap <cadn@erni.ph>
2018-10-01 09:07:14 +0800

    Removed back button title.

--- XF.Material/XF.Material.iOS/Renderers/MaterialNavigationPageRenderer.cs
+++ XF.Material/XF.Material.iOS/Renderers/MaterialNavigationPageRenderer.cs
@@ -1,9 +1,12 @@
 ﻿using CoreGraphics;
+using System;
 using UIKit;
+using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 using XF.Material.iOS.Renderers;
 using XF.Material.Forms.Views;
+using Xamarin.Forms.Internals;
 
 [assembly: ExportRenderer(typeof(MaterialNavigationPage), typeof(MaterialNavigationPageRenderer))]
 namespace XF.Material.iOS.Renderers
@@ -17,17 +20,24 @@ namespace XF.Material.iOS.Renderers
             if(e?.NewElement != null)
             {
                 var materialElement = this.Element as MaterialNavigationPage;
-                NavigationBar.Frame = new CGRect(0, 0, this.View.Frame.Size.Width, 56f);
+                this.NavigationBar.Frame = new CGRect(0, 0, this.View.Frame.Size.Width, 56f);
 
                 if(materialElement.HasShadow)
                 {
-                    NavigationBar.Layer.MasksToBounds = false;
-                    NavigationBar.Layer.ShadowColor = UIColor.Black.CGColor;
-                    NavigationBar.Layer.ShadowOffset = new CGSize(0, 3f);
-                    NavigationBar.Layer.ShadowOpacity = 0.32f;
-                    NavigationBar.Layer.ShadowRadius = 3f;
+                    this.NavigationBar.Layer.MasksToBounds = false;
+                    this.NavigationBar.Layer.ShadowColor = UIColor.Black.CGColor;
+                    this.NavigationBar.Layer.ShadowOffset = new CGSize(0, 3f);
+                    this.NavigationBar.Layer.ShadowOpacity = 0.32f;
+                    this.NavigationBar.Layer.ShadowRadius = 3f;
                 }
             }
+        }
+
+        public override void PushViewController(UIViewController viewController, bool animated)
+        {
+            base.PushViewController(viewController, animated);
+
+            viewController.NavigationItem.BackBarButtonItem = new UIBarButtonItem(string.Empty, UIBarButtonItemStyle.Plain, null);
         }
     }
 }
