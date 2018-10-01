a056fb9a15bcedc7a16938bfe2f639418f99d956
Dustin Catap <cadn@erni.ph>
2018-10-01 08:57:47 +0800

    Revert back changes.

--- XF.Material/XF.Material.Droid/Renderers/Internals/MaterialEntryRenderer.cs
+++ XF.Material/XF.Material.Droid/Renderers/Internals/MaterialEntryRenderer.cs
@@ -4,7 +4,6 @@ using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 using XF.Material.Droid.Renderers.Internals;
 using XF.Material.Forms.Views.Internals;
-using XF.Material.Droid;
 
 [assembly: ExportRenderer(typeof(MaterialEntry), typeof(MaterialEntryRenderer))]
 namespace XF.Material.Droid.Renderers.Internals
@@ -19,11 +18,8 @@ namespace XF.Material.Droid.Renderers.Internals
 
             if (e?.NewElement != null)
             {
-                this.Element.VerticalOptions = LayoutOptions.FillAndExpand;
-                this.Element.Margin = new Thickness(12, 0);
                 this.Control.Background = new ColorDrawable(Color.Transparent.ToAndroid());
-                this.Control.SetPadding(0, 0, 0, (int) MaterialHelper.ConvertToDp(12));
-                this.Control.Gravity = Android.Views.GravityFlags.Bottom;
+                this.Control.SetPadding(0, 0, 0, 0);
                 this.Control.SetIncludeFontPadding(false);
             }
         }
