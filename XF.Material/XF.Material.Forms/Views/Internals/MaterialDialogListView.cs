4263437de0467f8e0478ad9095836400e5cce711
Dustin Catap <cadn@erni.ph>
2018-10-01 09:11:38 +0800

    Added internal view used in dialog list.

---
+++ XF.Material/XF.Material.Forms/Views/Internals/MaterialDialogListView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF.Material.Forms.Views.Internals
+{
+    /// <summary>
+    /// Used for rendering the <see cref="Xamarin.Forms.ListView"/> control in <see cref="XF.Material.Forms.Dialogs.MaterialSimpleDialog"/>.
+    /// </summary>
+    public sealed class MaterialDialogListView : ListView
+    {
+        public Command<int> ItemSelectedCommand { get; set; }
+
+        public bool HasRipple { get; set; } = true;
+
+        internal MaterialDialogListView() { }
+
+        internal void SetBinding(BindableProperty itemsSourceProperty, object sourceProperty)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ XF.Material/XF.Material.Forms/Views/Internals/MaterialDialogListViewCell.cs
@@ -0,0 +1,9 @@
+﻿using Xamarin.Forms;
+
+namespace XF.Material.Forms.Views.Internals
+{
+    public class MaterialDialogListViewCell : ViewCell
+    {
+        internal MaterialDialogListViewCell() { }
+    }
+}
