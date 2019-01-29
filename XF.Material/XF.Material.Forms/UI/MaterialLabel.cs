936b788261f486eb558cb62b841f7a28d96e371a
Dustin Catap <dustinboy619@gmail.com>
2019-01-30 06:21:12 +0800

    Code refactor.

--- XF.Material/XF.Material.Forms/UI/MaterialLabel.cs
+++ XF.Material/XF.Material.Forms/UI/MaterialLabel.cs
@@ -7,33 +7,40 @@ namespace XF.Material.Forms.UI
 {
     public class MaterialLabel : Label
     {
-        public const string MaterialLineHeight = "MaterialLineHeight";
-
-        public static new readonly BindableProperty LineHeightProperty = BindableProperty.Create(MaterialLineHeight, typeof(double), typeof(MaterialLabel), 1.4);
+        public const string MaterialLineHeightPropertyName = "MaterialLineHeight";
 
         public static readonly BindableProperty LetterSpacingProperty = BindableProperty.Create(nameof(LetterSpacing), typeof(double), typeof(MaterialLabel), 0.0);
-
+        public static new readonly BindableProperty LineHeightProperty = BindableProperty.Create(MaterialLineHeightPropertyName, typeof(double), typeof(MaterialLabel), 1.4);
         public static readonly BindableProperty TypeScaleProperty = BindableProperty.Create(nameof(TypeScale), typeof(MaterialTypeScale), typeof(MaterialLabel), MaterialTypeScale.None);
 
-        private bool _fontSizeChanged;
         private bool _fontFamilyChanged;
+        private bool _fontSizeChanged;
         private bool _letterSpacingChanged;
 
+        /// <summary>
+        /// Gets or sets the letter spacing of this label's text.
+        /// </summary>
         public double LetterSpacing
         {
             get => (double)this.GetValue(LetterSpacingProperty);
             set => this.SetValue(LetterSpacingProperty, value);
         }
 
+        /// <summary>
+        /// Gets or sets the multiplier that determines the line height of this label.
+        /// </summary>
         public new double LineHeight
         {
             get => (double)this.GetValue(LineHeightProperty);
             set => this.SetValue(LineHeightProperty, value);
         }
 
+        /// <summary>
+        /// Gets or sets the type scale used for this label.
+        /// </summary>
         public MaterialTypeScale TypeScale
         {
-            get => (MaterialTypeScale) this.GetValue(TypeScaleProperty);
+            get => (MaterialTypeScale)this.GetValue(TypeScaleProperty);
             set => this.SetValue(TypeScaleProperty, value);
         }
 
@@ -41,12 +48,12 @@ namespace XF.Material.Forms.UI
         {
             base.OnPropertyChanged(propertyName);
 
-            if(propertyName == nameof(this.TypeScale))
+            if (propertyName == nameof(this.TypeScale))
             {
                 this.OnTypeScaleChanged(this.TypeScale);
             }
 
-            if(propertyName == nameof(this.FontSize))
+            if (propertyName == nameof(this.FontSize))
             {
                 _fontSizeChanged = true;
             }
@@ -62,32 +69,26 @@ namespace XF.Material.Forms.UI
             }
         }
 
-        protected void OnTypeScaleChanged(MaterialTypeScale materialTypeScale)
+        protected virtual void OnTypeScaleChanged(MaterialTypeScale materialTypeScale)
         {
-            if(materialTypeScale == MaterialTypeScale.None)
+            if (materialTypeScale == MaterialTypeScale.None)
             {
                 return;
             }
-
-            var letterSpacingKey = $"Material.LetterSpacing.{materialTypeScale.ToString()}";
-            
-            if (!_fontSizeChanged)
+            if (!_letterSpacingChanged)
             {
+                var letterSpacingKey = $"Material.LetterSpacing.{materialTypeScale.ToString()}";
                 this.LetterSpacing = Convert.ToDouble(Application.Current.Resources[letterSpacingKey]);
             }
-
-            var fontFamilyKey = $"Material.FontFamily.{materialTypeScale.ToString()}";
-
-            if (!_fontSizeChanged)
+            if (!_fontFamilyChanged)
             {
+                var fontFamilyKey = $"Material.FontFamily.{materialTypeScale.ToString()}";
                 this.FontFamily = Application.Current.Resources[fontFamilyKey].ToString();
             }
-
-            var fontSizeKey = $"Material.FontSize.{materialTypeScale.ToString()}";
-
-            if(!_fontSizeChanged)
+            if (!_fontSizeChanged)
             {
-                this.FontSize = Convert.ToDouble(Application.Current.Resources[fontSizeKey]); ;
+                var fontSizeKey = $"Material.FontSize.{materialTypeScale.ToString()}";
+                this.FontSize = Convert.ToDouble(Application.Current.Resources[fontSizeKey]);
             }
         }
     }
