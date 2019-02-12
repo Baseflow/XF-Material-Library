1a65fa02f3e6028831b9567c94f033cf03c42755
Dustin Catap <dustinboy619@gmail.com>
2019-02-13 06:10:08 +0800

    Added MaterialSwitch

---
+++ XF.Material/XF.Material.Forms/UI/MaterialSwitch.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF.Material.Forms.UI
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MaterialSwitch : ContentView
+    {
+        public static readonly BindableProperty ActiveBackgroundColorProperty = BindableProperty.Create(nameof(ActiveBackgroundColor), typeof(Color), typeof(MaterialSwitch), Material.Color.Secondary.MultiplyAlpha(0.54));
+
+        public static readonly BindableProperty ActiveThumbColorProperty = BindableProperty.Create(nameof(ActiveThumbColor), typeof(Color), typeof(MaterialSwitch), Material.Color.Secondary);
+
+        public static readonly BindableProperty InactiveBackgroundColorProperty = BindableProperty.Create(nameof(InactiveBackgroundColor), typeof(Color), typeof(MaterialSwitch), Color.LightGray);
+
+        public static readonly BindableProperty InactiveThumColorProperty = BindableProperty.Create(nameof(InactiveThumbColor), typeof(Color), typeof(MaterialSwitch), Color.FromHex("#FFFFFF"));
+
+        public static readonly BindableProperty IsActivatedProperty = BindableProperty.Create(nameof(IsActivated), typeof(bool), typeof(MaterialSwitch), false, BindingMode.TwoWay);
+
+        public MaterialSwitch()
+        {
+            this.InitializeComponent();
+            _background.Color = this.IsActivated ? this.ActiveBackgroundColor : this.InactiveBackgroundColor;
+        }
+
+        public event EventHandler<ActivatedEventArgs> Activated;
+
+        public Color ActiveBackgroundColor
+        {
+            get => (Color)this.GetValue(ActiveBackgroundColorProperty);
+            set => this.SetValue(ActiveBackgroundColorProperty, value);
+        }
+
+        public Color ActiveThumbColor
+        {
+            get => (Color)this.GetValue(ActiveThumbColorProperty);
+            set => this.SetValue(ActiveThumbColorProperty, value);
+        }
+
+        public Color InactiveBackgroundColor
+        {
+            get => (Color)this.GetValue(InactiveBackgroundColorProperty);
+            set => this.SetValue(InactiveBackgroundColorProperty, value);
+        }
+
+        public Color InactiveThumbColor
+        {
+            get => (Color)this.GetValue(InactiveThumColorProperty);
+            set => this.SetValue(InactiveThumColorProperty, value);
+        }
+
+        public bool IsActivated
+        {
+            get => (bool)this.GetValue(IsActivatedProperty);
+            set => this.SetValue(IsActivatedProperty, value);
+        }
+
+        protected override async void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+
+            if (propertyName == nameof(this.IsActivated))
+            {
+                this.OnActivatedChanged(this.IsActivated);
+            }
+
+            if (propertyName == nameof(this.ActiveBackgroundColor) || propertyName == nameof(this.InactiveBackgroundColor))
+            {
+                _background.Color = this.IsActivated ? this.ActiveBackgroundColor : this.InactiveBackgroundColor;
+            }
+        }
+
+        protected virtual void OnActivatedChanged(bool isActivated)
+        {
+            this.Activated?.Invoke(this, new ActivatedEventArgs(this.IsActivated));
+            Device.BeginInvokeOnMainThread(async () => await this.OnIsActivatedChanged(this.IsActivated));
+        }
+
+        private async Task AnimateToActivatedState()
+        {
+            await _thumb.TranslateTo(16, 0, 150, Easing.SinOut);
+            _thumb.BackgroundColor = this.ActiveThumbColor;
+        }
+
+        private async Task AnimateToUnactivatedState()
+        {
+            await _thumb.TranslateTo(0, 0, 100, Easing.SinOut);
+            _thumb.BackgroundColor = this.InactiveThumbColor;
+        }
+
+        private async Task OnIsActivatedChanged(bool isActivated)
+        {
+            if (isActivated)
+            {
+                await this.AnimateToActivatedState();
+            }
+            else
+            {
+                await this.AnimateToUnactivatedState();
+            }
+
+            _background.Color = this.IsActivated ? this.ActiveBackgroundColor : this.InactiveBackgroundColor;
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            this.IsActivated = !this.IsActivated;
+        }
+    }
+
+    public class ActivatedEventArgs : ToggledEventArgs
+    {
+        public ActivatedEventArgs(bool value) : base(value)
+        {
+        }
+    }
+}
