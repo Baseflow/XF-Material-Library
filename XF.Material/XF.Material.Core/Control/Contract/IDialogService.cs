5f42a195208aaf33ff0e286752fbb07ff8422263
Fiodor Songurov <songurov@gmail.com>
2019-02-19 17:04:46 +0200

    create abstration layer Core

---
+++ XF.Material/XF.Material.Core/Control/Contract/IDialogService.cs
@@ -0,0 +1,131 @@
+﻿//
+//  Author:
+//    Songurov Fiodor songurov@gmail.com
+//
+//  Copyright (c) 2019, 
+//
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in
+//       the documentation and/or other materials provided with the distribution.
+//     * Neither the name of the [ORGANIZATION] nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+//  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace XF.Material.Core.Control.Contract
+{
+    /// <summary>
+    /// Interface that defines the methods for showing dialogs and snackbars.
+    /// </summary>
+    public interface IDialogService
+    {
+        /// <summary>
+        /// Shows an alert dialog for acknowledgement. It only has a single, dismissive action used for acknowledgement.
+        /// </summary>
+        /// <param name="message">The message of the alert dialog.</param>
+        Task AlertAsync(string message);
+
+        /// <summary>
+        /// Shows an alert dialog for acknowledgement. It only has a single, dismissive action used for acknowledgement.
+        /// </summary>
+        /// <param name="message">The message of the alert dialog.</param>
+        /// <param name="title">The title of the alert dialog.</param>
+        Task AlertAsync(string message, string title);
+
+        /// <summary>
+        /// Shows an alert dialog for acknowledgement. It only has a single, dismissive action used for acknowledgement.
+        /// </summary>
+        /// <param name="message">The message of the alert dialog.</param>
+        /// <param name="title">The title of the alert dialog.</param>
+        /// <param name="acknowledgementText">The text of the acknowledgement button.</param>
+        Task AlertAsync(string message, string title, string acknowledgementText);
+
+        /// <summary>
+        /// Shows an alert dialog for confirmation. Returns true when the confirm button was clicked, false if the dismiss button was clicked, and null if the alert dialog was dismissed.
+        /// </summary>
+        /// <param name="message">The message of the alert dialog.</param>
+        /// <param name="title">The title of the alert dialog.</param>
+        /// <param name="confirmingText">The text of the confirmation button.</param>
+        /// <param name="dismissiveText">The text of the dismissive button</param>
+        Task<bool?> ConfirmAsync(string message, string title = null, string confirmingText = "Ok", string dismissiveText = "Cancel");
+
+        /// <summary>
+        /// Shows a confirmation dialog that allow users to input text. If confirmed, returns the text value of the textfield. If canceled or dismissed, returns <see cref="string.Empty"/>.
+        /// </summary>
+        /// <param name="title">The title of the confirmation dialog.</param>
+        /// <param name="message">The message of the confirmation dialog.</param>
+        /// <param name="inputText">The initial text of the textfield.</param>
+        /// <param name="inputPlaceholder">The placeholder of the textfield.</param>
+        /// <param name="confirmingText">The text of the confirmation button.</param>
+        /// <param name="dismissiveText">The text of the dismissive button</param>
+        Task<string> InputAsync(string title = null, string message = null, string inputText = null, string inputPlaceholder = "Enter input", string confirmingText = "Ok", string dismissiveText = "Cancel");
+
+        /// <summary>
+        /// Shows a simple dialog that allows the user to select one of listed actions. Returns the index of the selected action.
+        /// </summary>
+        /// <param name="actions">The list of actions.</param>
+        /// <exception cref="ArgumentNullException" />
+        Task<int> SelectActionAsync(IList<string> actions);
+
+        /// <summary>
+        /// Shows a simple dialog that allows the user to select one of listed actions. Returns the index of the selected action.
+        /// </summary>
+        /// <param name="title">The title of the dialog.</param>
+        /// <param name="actions">The list of actions.</param>
+        /// <exception cref="ArgumentNullException" />
+        Task<int> SelectActionAsync(string title, IList<string> actions);
+
+        /// <summary>
+        /// Shows a confirmation dialog that allows the user to select one of the listed choices. Returns the index of the selected choice. If none was selected, returns -1.
+        /// </summary>
+        /// <param name="title">The title of the confirmation dialog. This parameter must not be null or empty.</param>
+        /// <param name="choices">The list of choices the user will choose from.</param>
+        /// <exception cref="ArgumentNullException" />
+        Task<int> SelectChoiceAsync(string title, IList<string> choices);
+
+        /// <summary>
+        /// Shows a confirmation dialog that allows the user to select one of the listed choices. Returns the index of the selected choice. If none was selected, returns -1.
+        /// </summary>
+        /// <param name="title">The title of the confirmation dialog. This parameter must not be null or empty.</param>
+        /// <param name="choices">The list of choices the user will choose from.</param>
+        /// <param name="selectedIndex">The currently selected index.</param>
+        /// <exception cref="ArgumentNullException" />
+        /// <exception cref="IndexOutOfRangeException" />
+        Task<int> SelectChoiceAsync(string title, IList<string> choices, int selectedIndex);
+
+        /// <summary>
+        /// Shows a confirmation dialog that allows the user to select any of the listed choices. Returns the indices of the selected choices. If none was selected, returns an empty array.
+        /// </summary>
+        /// <param name="title">The title of the confirmation dialog. This parameter must not be null or empty.</param>
+        /// <param name="choices">The list of choices the user will choose from.</param>
+        /// <exception cref="ArgumentNullException" />
+        Task<int[]> SelectChoicesAsync(string title, IList<string> choices);
+
+        /// <summary>
+        /// Shows a confirmation dialog that allows the user to select any of the listed choices. Returns the indices of the selected choices. If none was selected, returns an empty array.
+        /// </summary>
+        /// <param name="title">The title of the confirmation dialog. This parameter must not be null or empty.</param>
+        /// <param name="choices">The list of choices the user will choose from.</param>
+        /// <param name="selectedIndices">The currently selected indices.</param>
+        /// <exception cref="ArgumentNullException" />
+        /// <exception cref="IndexOutOfRangeException" />
+        Task<int[]> SelectChoicesAsync(string title, IList<string> choices, List<int> selectedIndices);
+    }
+}
