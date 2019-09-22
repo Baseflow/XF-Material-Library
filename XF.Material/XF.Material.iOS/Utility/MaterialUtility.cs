b0b0c3db806bbbb60a7e19579fd0d32997e5b59f
Pepsi1x1 <davyt23@hotmail.co.uk>
2019-09-22 17:56:52 +0100

    Workaround for crash when setting the status bar color in iOS 13

--- XF.Material/XF.Material.iOS/Utility/MaterialUtility.cs
+++ XF.Material/XF.Material.iOS/Utility/MaterialUtility.cs
@@ -16,9 +16,40 @@ namespace XF.Material.iOS.Utility
     {
         public void ChangeStatusBarColor(Color color)
         {
-            var isColorDark = color.ToCGColor().IsColorDark();
-            UIApplication.SharedApplication.StatusBarStyle = isColorDark ? UIStatusBarStyle.LightContent : UIStatusBarStyle.Default;
-            if (UIApplication.SharedApplication.ValueForKey(new NSString("statusBar")) is UIView statusBar) statusBar.BackgroundColor = color.ToUIColor();
+            bool isColorDark = color.ToCGColor().IsColorDark();
+            //Status Bar property removed in iOS 13
+            //TODO: Implement statusBarManager when bindings available from Xamarin
+            //if (@available(iOS 13, *))
+            //{
+            //    UIView* statusBar = [[UIView alloc]initWithFrame:[UIApplication sharedApplication].keyWindow.windowScene.statusBarManager.statusBarFrame] ;
+            //    statusBar.backgroundColor = [UIColor redColor];
+            //    [[UIApplication sharedApplication].keyWindow addSubview:statusBar];
+            //}
+            if (UIDevice.CurrentDevice.CheckSystemVersion(13, 0))
+            {
+                System.nint tag = (System.nint)38482458385;
+                UIView statusBar = UIApplication.SharedApplication.KeyWindow?.ViewWithTag(tag);
+                if (statusBar != null)
+                {
+                    statusBar.BackgroundColor = color.ToUIColor();
+                }
+                else
+                {
+                    UIView statusBarView = new UIView(frame: UIApplication.SharedApplication.StatusBarFrame);
+                    statusBarView.Tag = tag;
+
+                    UIApplication.SharedApplication.KeyWindow?.AddSubview(statusBarView);
+                    statusBarView.BackgroundColor = color.ToUIColor();
+                }
+            }
+            else
+            {
+                UIApplication.SharedApplication.StatusBarStyle = isColorDark ? UIStatusBarStyle.LightContent : UIStatusBarStyle.Default;
+                if (UIApplication.SharedApplication.RespondsToSelector(new ObjCRuntime.Selector("statusBar")))
+                {
+                    if (UIApplication.SharedApplication.ValueForKey(new NSString("statusBar")) is UIView statusBar) statusBar.BackgroundColor = color.ToUIColor();
+                }
+            }
         }
     }
 }
