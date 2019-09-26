2e668ab6643857cb5aafe61ddf06234a5ad18e3b
Mateus Henrique <mtsrdrr@gmail.com>
2019-09-26 17:18:08 -0300

    Fixed NullReferenceException when trying to change the status bar color in iOS 13.

---
+++ XF.Material/XF.Material.iOS/DeviceHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UIKit;
+
+namespace XF.Material.iOS
+{
+    internal static class DeviceHelper
+    {
+        internal static bool IsiOS13OrNewer
+        {
+            get
+            {
+                return UIDevice.CurrentDevice.CheckSystemVersion(13, 0);
+            }
+        }
+    }
+}
--- XF.Material/XF.Material.iOS/Utility/MaterialUtility.cs
+++ XF.Material/XF.Material.iOS/Utility/MaterialUtility.cs
@@ -1,4 +1,5 @@
-﻿using Foundation;
+﻿using System.Diagnostics;
+using Foundation;
 using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
@@ -17,8 +18,25 @@ namespace XF.Material.iOS.Utility
         public void ChangeStatusBarColor(Color color)
         {
             var isColorDark = color.ToCGColor().IsColorDark();
-            UIApplication.SharedApplication.StatusBarStyle = isColorDark ? UIStatusBarStyle.LightContent : UIStatusBarStyle.Default;
-            if (UIApplication.SharedApplication.ValueForKey(new NSString("statusBar")) is UIView statusBar) statusBar.BackgroundColor = color.ToUIColor();
+
+            if (DeviceHelper.IsiOS13OrNewer)
+            {
+                if (UIApplication.SharedApplication.KeyWindow == null)
+                {
+                    //Debug.WriteLine("ChangeStatusBarColor(color): UIApplication.SharedApplication.KeyWindow is null - This operation cannot be complete until KeyWindow is setted.");
+                    return;
+                }
+
+                UIApplication.SharedApplication.StatusBarStyle = isColorDark ? UIStatusBarStyle.LightContent : UIStatusBarStyle.Default;
+                UIView statusBar = new UIView(UIApplication.SharedApplication.KeyWindow.WindowScene.StatusBarManager.StatusBarFrame);
+                statusBar.BackgroundColor = color.ToUIColor();
+                UIApplication.SharedApplication.KeyWindow.AddSubview(statusBar);
+            }
+            else
+            {
+                UIApplication.SharedApplication.StatusBarStyle = isColorDark ? UIStatusBarStyle.LightContent : UIStatusBarStyle.Default;
+                if (UIApplication.SharedApplication.ValueForKey(new NSString("statusBar")) is UIView statusBar) statusBar.BackgroundColor = color.ToUIColor();
+            }
         }
     }
 }
