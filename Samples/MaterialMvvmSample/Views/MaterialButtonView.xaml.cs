5220cc43d026566ab1d680d4efa770244ed8ec57
Odette Oosterkamp <the.flower.of.life@gmail.com>
2019-12-02 13:01:57 +0100

    #224 reproducing Error in Dialog. Added button with dialog and choices

--- Samples/MaterialMvvmSample/Views/MaterialButtonView.xaml.cs
+++ Samples/MaterialMvvmSample/Views/MaterialButtonView.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using MaterialMvvmSample.ViewModels;
 using Xamarin.Forms;
 using XF.Material.Forms.UI;
+using XF.Material.Forms.UI.Dialogs;
 
 namespace MaterialMvvmSample.Views
 {
@@ -17,6 +18,29 @@ namespace MaterialMvvmSample.Views
         private void Btn_Clicked(object sender, EventArgs e)
         {
             ((MaterialButton)sender).Text = "Clicked 1 Time";
+        }
+
+        private async void MaterialButton_Clicked(object sender, EventArgs e)
+        {
+            var jobs = new string[]
+            {
+            "Mobile Developer (Xamarin)",
+            "Mobile Developer (Native)",
+            "Web Developer (.NET)",
+            "Web Developer (Laravel)",
+            "Quality Assurance Engineer",
+            "Business Analyst",
+            "Recruitment Officer",
+            "Project Manager",
+            "Scrum Master"
+            };
+            await MaterialDialog.Instance.AlertAsync(message: "This is an alert dialog",
+                                                            title: "Alert Dialog",
+                                                            acknowledgementText: "Got It");
+            //Show confirmation dialog for choosing one.
+            var result = await MaterialDialog.Instance.SelectChoicesAsync(title: "Select a job",
+                selectedIndices: new int[] { 1, 6 },
+                                                              choices: jobs);
         }
     }
 }
