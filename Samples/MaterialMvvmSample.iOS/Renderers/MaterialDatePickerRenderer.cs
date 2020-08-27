d51eba49f7bb82fa485c3c4a5abbb73b060ad4c0
Benjamin Mayrargue <benjamin@vapolia.fr>
2020-08-27 08:01:07 +0200

    rewrite materialdatepicker so it works correctly on iOS

---
+++ Samples/MaterialMvvmSample.iOS/Renderers/MaterialDatePickerRenderer.cs
@@ -0,0 +1,33 @@
+﻿using MaterialMvvmSample.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XF.Material.Forms.UI.Internals;
+
+[assembly:ExportRenderer(typeof(MaterialDatePicker), typeof(MaterialDatePickerRenderer))]
+
+namespace MaterialMvvmSample.iOS.Renderers
+{
+    /// <summary>
+    /// Remove border
+    /// </summary>
+    public class MaterialDatePickerRenderer : DatePickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+            {
+                Control.Layer.BorderWidth = 0;
+                Control.BorderStyle = UITextBorderStyle.None;
+            }
+        }
+
+        protected override UITextField CreateNativeControl()
+        {
+            var control = base.CreateNativeControl();
+            control.BorderStyle = UITextBorderStyle.None;
+            return control;
+        }
+    }
+}
